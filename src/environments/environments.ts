7df7375dbe12adfbbfdb35984f65dbe95e45614c
serviceMonetique <servicemonetiquetn@gmail.com>
2023-12-08 00:00:06 +0100

    dfghjklm

---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase:{
+    apiKey: "AIzaSyAEB5dS_7mPcGE1qMJNdCXxx4yceFTjOF8",
+    authDomain: "monetiqued17.firebaseapp.com",
+    projectId: "monetiqued17",
+    storageBucket: "monetiqued17.appspot.com",
+    messagingSenderId: "452700936556",
+    appId: "1:452700936556:web:c9578c345c8b42f9277b49",
+    measurementId: "G-QH60NEE9XM"
+  },
+  production: false,
+}
