0397ab7dc86bd070f58560aeb8ed0fe0f4ce6692
serviceMonetique <servicemonetiquetn@gmail.com>
2023-12-08 01:28:14 +0100

    Revert "fghjklm"
    This reverts commit 6015e964

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import { TableComponent } from './table/table.component';
-
-
-import {provideFirebaseApp,initializeApp} from "@angular/fire/app";
-import { provideFirestore,getFirestore} from "@angular/fire/firestore";
+import {initializeApp} from "@angular/fire/app";
+import {provideFirebaseApp} from "@angular/fire/app";
+import {getFirestore, provideFirestore} from "@angular/fire/firestore";
 import {environment} from "../environments/environments";
 @NgModule({
   declarations: [
